7d35a4d9d97551f887c0996581682fdc926f222a
Umair-28 <h.umairamjad.30@gmail.com>
2025-09-02 17:44:58 +0500

    updated company name field

--- models/vicidial_leads.py
+++ models/vicidial_leads.py
@@ -5,7 +5,7 @@ class VicidialLead(models.Model):
     _description = "Vicidial Lead Data"
 
     stage_id = fields.Many2one('crm.stage', string="Stage")
-    company_name = fields.Char(string="Company Name")
+    company_name = fields.Many2one('res.company', string="Company")  # Correct relational field
     lead_id = fields.Char(string="Lead ID", index=True)
     status = fields.Char(string="Status")
     entry_date = fields.Datetime(string="Entry Date")
