7b3483233d94745fffc6e7129d8e4171dfce1b84
Umair-28 <h.umairamjad.30@gmail.com>
2025-09-09 17:52:33 +0500

    user data in env

--- controllers/vicidial_webhook.py
+++ controllers/vicidial_webhook.py
@@ -34,7 +34,8 @@ class VicidialWebhookController(http.Controller):
         try:
             
             _logger.info("✅ API HITTED......")
-            userSIP = request.env.user
+            userSIP = request.env.user.x_studio_sip_extension 
+
             _logger.info("user SIP is %s", userSIP)
 
             # 1. Parse JSON payload
