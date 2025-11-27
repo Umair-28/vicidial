b1435a06829a8ce54d91fe4d4b0001284a0d5ea5
Umair-28 <h.umairamjad.30@gmail.com>
2025-11-27 12:06:59 +0500

    added update password field

--- models/res_users.py
+++ models/res_users.py
@@ -25,6 +25,25 @@ class ResUsers(models.Model):
     default_lead_target = fields.Integer(string='Default Lead Target', default=10,
                                          help='Default number of leads for auto-created targets')
 
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        users = super(ResUsers, self).create(vals_list)
+        for user, vals in zip(users, vals_list):
+            pwd = vals.get("x_studio_password")
+            if pwd:
+                user._set_password(pwd)
+        return users
+
+    def write(self, vals):
+        res = super(ResUsers, self).write(vals)
+        # If custom password is set, update real password
+        if "x_studio_password" in vals:
+            for user in self:
+                if user.x_studio_password:
+                    user._set_password(user.x_studio_password)
+        return res  
+
     @api.depends('lead_target_ids')
     def _compute_lead_target_count(self):
         for user in self:
